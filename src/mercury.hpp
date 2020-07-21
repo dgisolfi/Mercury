aba2c2ab77aa1c5aca3109e71bf669b1b7391c9a
Daniel-Gisolfi <Daniel.Gisolfi@ibm.com>
2020-07-20 21:19:35 -0400

    Added link to mercury file

--- src/mercury.hpp
+++ src/mercury.hpp
@@ -3,8 +3,9 @@
  * @Description: Lightweight & Colorful Singleton logger
  * @Version: 1.0.0
  * 
- * This is a single header lib, get the latest build from: 
- * Include this file in your project to start using Mercury
+ * This is a single header lib, get the latest build from the following 
+ * link. Include this file in your project to start using Mercury!
+ * https://raw.githubusercontent.com/dgisolfi/Mercury/master/single_include/mercury.hpp
  * 
  * @Date: 2020-7-20
  * @Author Daniel Nicolas Gisolfi
